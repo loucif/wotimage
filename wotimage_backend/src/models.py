92f7e25c175cbc11aafb43ae94815602cef0e8a9
loucif <loucif.ft13@gmail.com>
2021-08-07 17:51:13 +0100

    feat: add classification

--- wotimage_backend/src/models.py
+++ wotimage_backend/src/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from keras_preprocessing.image import load_img, img_to_array
+from keras_preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.inception_resnet_v2 import decode_predictions, preprocess_input, InceptionResNetV2
+import numpy as np
 
 # Create your models here.
+
+
 class Image(models.Model):
     picture = models.ImageField()
     classified = models.CharField(max_length=200, blank=True)
@@ -9,3 +15,19 @@ class Image(models.Model):
 
     def __str__(self):
         return "Image classified at {}".format(timezone.localtime(self.uploaded).strftime('%Y-%m-%d %H:%M'))
+
+    def save(self, *args, **kwargs):
+        try:
+            print(self.picture.path)
+            img = load_img(self.picture.path, target_size=(299, 299))
+            print(img)
+            img_array = img_to_array(img)
+            to_predict = np.expand_dims(img_array, axis=0)
+            preprocess = preprocess_input(to_predict)
+            model = InceptionResNetV2(weights='imagenet')
+            predication = model.predict(preprocess)
+            decode = decode_predictions(predication)
+            print(decode)
+        except Exception as e:
+            print(e)
+        super().save(*args, **kwargs)
